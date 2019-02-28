cac5fb3f90f98d1f68bcdb7822acffd800747880
Mohamed Baraka <mbaraka@kobo.com>
2019-02-28 03:46:17 -0500

    add loading to forecast activity and handle error

--- app/src/main/java/com/weather/CityDetails/CityActivity.kt
+++ app/src/main/java/com/weather/CityDetails/CityActivity.kt
@@ -2,12 +2,16 @@ package com.weather.CityDetails
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.weather.R
 import com.weather.home.CitiesRecyclerAdapter
 import com.weather.model.OpenWeather
+import com.weather.utils.loge
 import com.weather.utils.rxError
 import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.activity_city.*
@@ -42,12 +46,17 @@ class CityActivity : AppCompatActivity() {
         activityCity_recycler.addItemDecoration(dividerItemDecoration)
         activityCity_recycler.adapter = citiesRecyclerAdapter
 
+        progress.visibility = View.VISIBLE
         val presenter = CityDetailsPresenter(openWeather.id)
         presenter.requestDataForCities()
-                .subscribe(Consumer {
+                .subscribe({
+                    progress.visibility = View.GONE
                     toolbar.title = it.city.name
                     citiesRecyclerAdapter.addItem(it.list)
-                }, rxError("failed to request the forecast for the city"))
+                }, {
+                    progress.visibility = View.GONE
+                    Toast.makeText(this, "failed to request the forecast for the city", Toast.LENGTH_LONG).show()
+                })
 
     }
 
